a922119dda40acec2a66b0bcec9c5a36bddea387
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-08-01 10:57:19 +0300

    Add files via upload

---
+++ DebuggerDemo/src/com/company/StringUtilities.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class StringUtilities {
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+    public void addChar(StringBuilder sBuilder, char c){
+        this.sBuilder.append(c);
+        charsAdded++;
+    }
+}
