54f5144c7beb37c8b955e5fef0a55c3651c3ab48
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-08-05 09:28:08 +0300

    Add files via upload

---
+++ JUnit Challenge/src/com/company/UtilitiesTest.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @org.junit.Test
+    public void everyNthChar() {
+        fail("This test has not been implemented");
+    }
+
+    @org.junit.Test
+    public void removePairs() {
+       Utilities util = new Utilities();
+       assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
+       assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
+
+    }
+
+    @org.junit.Test
+    public void converter() {
+        fail("This test has not been implemented");
+    }
+
+    @org.junit.Test
+    public void nullIfOddLength() {
+        fail("This test has not been implemented");
+    }
+
+}
